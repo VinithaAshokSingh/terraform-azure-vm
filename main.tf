8e6c8d7aad487448288bceb93fee9fa953f45ce8
VinithaAshokSingh <vinithashokunal@gmail.com>
2026-01-18 05:53:14 -0800

    Move terraform and provider blocks to main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,28 @@
-provider "azurerm"{ 
-  features {}
+terraform {
+  required_version = ">= 1.0"
+  
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.85.0"  
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+    virtual_machine {
+      delete_os_disk_on_deletion     = true
+      graceful_shutdown              = false
+      skip_shutdown_and_force_delete = false
+    }
+    key_vault {
+      purge_soft_delete_on_destroy = false
+    }
+  }
 }
 
 resource "azurerm_resource_group" "rg" {
