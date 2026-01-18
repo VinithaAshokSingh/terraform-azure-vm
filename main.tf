f310bde26917a52dcde761ec87d4e3952c6bae7e
VinithaAshokSingh <vinithashokunal@gmail.com>
2026-01-18 23:59:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "Vini-VM"
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
-  size                = "Standard_B2ats_v2"
+  size                = "Standard_B4ms"
   admin_username      = "azureuser"
   network_interface_ids = [
     azurerm_network_interface.nic.id
