68412523977b5219f3ab58d0ef822bfb8be6bc3e
VinithaAshokSingh <vinithashokunal@gmail.com>
2026-01-17 20:58:58 -0800

    Fix SSH key handling for Jenkins pipeline

--- variables.tf
+++ variables.tf
@@ -1,2 +1,13 @@
-variable "rg_name" {}
-variable "location" {}
+variable "rg_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key for VM login"
+  type        = string
+}
+
